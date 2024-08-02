5a6059a9e9b3350885b66b43e3d0bf229be8ab2c
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-08-02 16:42:43 -0700

    Updated port from 8080 to 8081

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ http.createServer((request, response) => {
 
   });
 
-}).listen(8080);
-console.log('My test server is running on Port 8080.');
+}).listen(8081);
+console.log('My test server is running on Port 8081.');
 
 setTimeout (() => {
   debugger; 
