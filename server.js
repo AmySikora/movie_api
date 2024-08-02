768f782e301eeeaac846510e9f13ce484d1aaded
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-08-01 21:00:51 -0700

    added buffer to test.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const http = require('http'),
+const http = require('http'); http.Agent
   fs = require('fs'),
   url = require('url');
 
--- test.js
+++ test.js
@@ -1,2 +1,7 @@
 console.log('Hello, Node!');
 console.log('Goodbye!');
+
+const bufs = Buffer.from([1, 2, 3, 4]);
+for (const buf of bufs) {
+console.log(buf);
+};
