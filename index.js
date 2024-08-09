b72e017391a8e4726284f5f10586950c9dc91b5c
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-08-09 16:17:36 -0700

    added server.js

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 const express = require ('express');
 const app = express(); 
 
-let topBooks = [
-    {
-        title: 'Harry Potter and the Sorcerer\'s Stone',
-        author: 'J.K. Rowling'
-    }, 
-    {
-        tile: 'Lord of the Rings',
-        author: 'J.R.R. Tolkien'
-    },
-    {
-        title: 'Twlight',
-        author: 'Stephaine Meyer'
-    }
-];
+const http = require('http');
 
-//GET requests
-app.get('/', (req, res) => {
-    res.send('Welcome to my book club!');
-});
+http.createServer((request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Welcome to my book club!\n');
+}).listen(8080);
 
-app.get('/documentation', (req, res) => {s
-    res.sendFile('public/documentation.html', { 
-        root: __dirname })
-});
-
-//listen for requests
-app.listen(8081, () => {
-    console.log('Your app is listening on port 8080.')
-});
+console.log('My first Node test server is running on Port 8080.');
