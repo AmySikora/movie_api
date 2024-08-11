a0314cc6ca82b76870a497e07adbad384e35d707
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-08-11 16:44:08 -0700

    added static files and error handing to index.js

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
+// Require express
 const express = require('express'),
   morgan = require('morgan');
-
 const app = express();
 
-app.use(morgan('common'));
+// Morgan used to log requests 
+app.use(morgan('combined'));
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my app!');
+// Add movie data
+app.get('/movies', (req, res) => {
+    const movies = [
+        { title: "The Shawshank Redemption", director: "Frank Darabont" },
+        { title: "The Godfather", director: "Francis Ford Coppola" },
+        { title: "The Dark Knight", director: "Christopher Nolan" },
+        { title: "Pulp Fiction", director: "Quentin Tarantino" },
+        { title: "Forrest Gump", director: "Robert Zemeckis" },
+        { title: "The Matrix", director: "The Wachowskis" },
+        { title: "Inception", director: "Christopher Nolan" },
+        { title: "Fight Club", director: "David Fincher" },
+        { title: "The Lord of the Rings: The Return of the King", director: "Peter Jackson" },
+        { title: "Star Wars: Episode V - The Empire Strikes Back", director: "Irvin Kershner" }
+    ];
+  res.json(movies);
 });
 
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
+app.get('/', (req, res) => {
+  res.send('Welcome to myFlix!.');
+});
+
+// Serve static files
+app.use(express.static('public'));
+
+// Create error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
 });
 
 app.listen(8080, () => {
