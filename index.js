fc79df4b5a413986e7cf0de93743a281163eafc5
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-08-14 22:06:18 -0700

    added an update user put

--- index.js
+++ index.js
@@ -91,6 +91,21 @@ app.post('/users', (req, res) => {
 
 })
 
+// Update User 
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body
+  
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.sendStatus(200).json(user); 
+  } else{
+    res.status(400).send('no such user') 
+  }
+})
+
 
 // READ movies
 app.get('/movies', (req, res) => {
