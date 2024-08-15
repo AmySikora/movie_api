eddffc9c34bffa7688c44fc2510a8fc3210f1d1e
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-08-14 23:44:50 -0700

    Updated a typo to get user delete to work

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ app.delete('/users/:id', (req, res) => {
   let user = users.find( user => user.id == id );
 
   if (user) {
-    users = user.filter( user => user.id != id);
+    users = users.filter( user => user.id != id);
     res.status(200).send(`user ${id} has been deleted`);
   } else {
       res.status(400).send('no such user')
