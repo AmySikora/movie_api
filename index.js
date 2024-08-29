8f79451aa4a8c5a3609ed043765be8f4dbae930b
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-08-29 13:10:03 -0700

    added code to create a user and check for errors

--- index.js
+++ index.js
@@ -90,6 +90,32 @@ let movies = [
     ];
 
 // Create users
+
+app.post('/users', async (req, res) => {
+  await Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.Status(400).send(req.bodyUsername + 'already exisits');
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body. Email,
+          Birthday: req.body.Birthday
+        })
+        .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        });
+      }
+    })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error:' + error);
+      });
+});
+
 app.post('/users', (req, res) => {
   const newUser = req.body;
   if (newUser.name) {
