943d769bbce5a5680423b2f45c28e0a8600b00b9
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-08-31 15:36:20 -0700

    updated mongoose.connect

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const Directors = Models.Director;
 
 // mongoose connect
 mongoose.connect('mongodb://localhost:27017/moviesDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 });
 
 // Require express, Morgan, body-parser, and uuid
