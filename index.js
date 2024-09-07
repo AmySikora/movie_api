7f957b8bed3e5fd174fb7f539de8a84860af9450
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-09-06 22:08:36 -0700

    UPdated mongoose connect

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ const Directors = Models.Director;
 
 // mongoose connect
 //mongoose.connect('mongodb://localhost:27017/dbname', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect(process.env.CONNECTION_URI, {
+const CONNECTION_URI = process.env.CONNECTION_URI; 
+
+mongoose.connect(CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => console.log('MongoDB connected successfully'))
-.catch(err => console.error('Error connecting to MongoDB:', err));
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Require express, Morgan, body-parser, and uuid
 const express = require('express');
