3f8c83e9a1f8ac3f65e040c5c9c9fb2f839727fe
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-10-22 13:51:22 -0700

    Update index.js to add netlify.app to allowed origins

--- index.js
+++ index.js
@@ -27,23 +27,19 @@ let allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:1234",
   "https://myflixmovies123-d3669f5b95da.herokuapp.com/",
-];
+  "https://myFlix-app-123.netlify.app",
+]; 
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn't found on the list of allowed origins}
+      let message = 'The CORS policy for this application does not alllow access from origin ' + origin;
+      return callback(new Error(message), false); 
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require("./auth")(app);
 
